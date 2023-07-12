f29348fb3e19da7cde7f2785771c4af4d1d4a4bb
gutamen <gutamen@live.com>
2023-07-12 16:48:04 -0300

    modificações estranhas

--- DatagramServer.java
+++ DatagramServer.java
@@ -5,12 +5,13 @@ import java.net.DatagramSocket;
 
 public class DatagramServer {
     static boolean first = true;
+    static Semaphore fila;
 
     public static void main(String[] args) {
         int packetSize = 500;
         byte[] buffer = new byte[packetSize];
         boolean stopFlag = false;
-
+        fila = new Semaphore(1);
         try (FileOutputStream fileOutputStream = new FileOutputStream("received_dubious_packets.txt");
              DatagramSocket serverSocket = new DatagramSocket(23456)) {
 
@@ -30,6 +31,26 @@ public class DatagramServer {
                     stopFlag = true;
                     break;
                 }
+
+                // Inicia uma nova thread para lidar com o pacote recebido
+                Thread clientThread = new Thread(() -> {
+                try {
+		                	    			
+                    // Obtém os dados recebidos
+                    byte[] data = receivePacket.getData().clone();
+                    
+                    fila.acquire(); 
+                    // Recebe o arquivo
+                    fileOutputStream.write(buffer, 0, receivePacket.getLength());
+                    fila.release();
+						
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                });
+
+                // Inicia a thread do cliente
+                clientThread.start();
 
                 fileOutputStream.write(buffer, 0, receivePacket.getLength());
             }
